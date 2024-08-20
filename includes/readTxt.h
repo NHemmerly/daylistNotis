26e450f4d5cac98654e8b2e3701e41a5327e146e
Neil Hemmerly <neilhemm0@gmail.com>
2024-08-19 23:25:03 -0400

    refactor and token refreshing code

--- includes/readTxt.h
+++ includes/readTxt.h
@@ -9,8 +9,11 @@ class Reader
         Reader() = default;
         const std::string& getText(const std::string& fileName)
         {
-
             std::string addPath = "../.secrets/" + fileName;
+            if (!(fileExists(addPath)))
+            {
+                std::ofstream {addPath};
+            }
             newFile.open(addPath, std::ios::in);
             if (newFile.is_open())
             {
@@ -26,7 +29,37 @@ class Reader
                 text = "unable to open file";
             }
             return text;
-        };
+        }
+
+        const bool fileExists (const std::string& fileName)
+        {
+            std::string addPath = "../.secrets/" + fileName;
+            if (FILE *file = fopen(addPath.c_str(), "r"))
+            {
+                fclose(file);
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        void writeText(const std::string& fileName, const std::string& text)
+        {
+            std::string addPath = "../.secrets/" + fileName;
+            if (fileExists(addPath))
+            {
+                newFile.open(addPath, std::ios::out);
+                if (newFile.is_open()){
+                    
+                    newFile << text;
+                    newFile.close();
+                } else {
+                    std::cout << "can't open file. Text:" << "\n" << text;
+                }
+            } else {
+                std::cout << "File does not exist" << std::endl;
+            }
+        }
     private:
         std::fstream newFile{};
         std::string text{};
