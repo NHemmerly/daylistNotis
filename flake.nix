6601e9a03484014684d9313b7ff128e393500948
Neil Hemmerly <neilhemm0@gmail.com>
2024-08-31 21:52:10 -0400

    update flake to build and run on Nixos

--- flake.nix
+++ flake.nix
@@ -29,6 +29,7 @@
               lcov
               vcpkg
               vcpkg-tool
+              openssl_3_3
             ] ++ (if system == "aarch64-darwin" then [ ] else [ gdb ]);
           };
       });
